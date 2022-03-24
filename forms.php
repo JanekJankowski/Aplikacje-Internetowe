6be6cdd79c228d5e29f9dffa45716e585cc71e46
JanekJankowski <100559844+JanekJankowski@users.noreply.github.com>
2022-03-24 17:54:21 +0100

    Add files via upload

--- forms.php
+++ forms.php
@@ -6,9 +6,16 @@
 </head>
 <body>
     <h4>Dane użytkownika</h4>
-    <form action="./scripts//form_data.php" method="POST">
-        <input type="text" name="name" placeholder="Podaj imię">
-        <input type="password" name="pass" placeholder="Podaj hasło">
+    <form action="./scripts/form_data.php" method="POST">
+        <input type="text" name="name" placeholder="Podaj imię"><br>
+        <input type="password" name="pass" placeholder="Podaj hasło"><br>
+        <select name="city">
+            <option value="poz">Poznań</option>
+            <option value="gn">Gniezno</option>
+        </select><br>
+        <input type="radio" name="gender" value="Człowiek">Człowiek<br>
+        <input type="radio" name="gender" value="Kobieta">Kobieta<br>
+        <input type="checkbox" name="terms">Regulamin<br>
         <input type="submit" value="Zatwierdź">
     </form>
 </body>
